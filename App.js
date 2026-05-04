fe5d27a92f87a9657518d9b5134afb1cfa756ba5
yogeshtripathi265-design <yogeshtripathi265@gmail.com>
2026-05-04 11:23:00 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function App(){
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-orange-100">
+      <h1 className="text-3xl font-bold text-orange-600">
+        Pooja Travel Agency 🚀
+      </h1>
+    </div>
+  );
+}
